498b949b2a2d8b4e2110c7bb31c87818f443e0a0
Joe Nash <joe.nash@getbraintree.com>
2015-09-01 11:05:04 +0100

    Add GoSquared

--- footer.php
+++ footer.php
@@ -20,7 +20,14 @@
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
+<script>
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
 
+  _gs('GSN-090772-U');
+</script>
 	<?php wp_footer(); ?>
 </body>
 </html>
